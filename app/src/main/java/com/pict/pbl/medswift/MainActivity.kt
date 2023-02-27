c62bceea7060e3963223e0002a9139608118be24
Advait Naik <ad.naik0801@gmail.com>
2023-02-27 17:07:42 +0530

    Added composables for Email and Password in MainActivity.kt

--- app/src/main/java/com/pict/pbl/medswift/MainActivity.kt
+++ app/src/main/java/com/pict/pbl/medswift/MainActivity.kt
@@ -3,13 +3,13 @@ package com.pict.pbl.medswift
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.constraintlayout.compose.ConstraintLayout
 import com.pict.pbl.medswift.ui.theme.MedSwiftTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,11 +22,48 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Text(text = "Hello Python")
+                    ActivityUI()
 
                 }
             }
         }
     }
+
+    @Preview
+    @Composable
+    private fun ActivityUI(){
+        Column (
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+            EmailId(modifier = Modifier.fillMaxWidth())
+            Password(modifier = Modifier.fillMaxWidth())
+
+        }
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    private fun EmailId( modifier: Modifier ){
+        var email by remember{ mutableStateOf("") }
+        TextField(
+            modifier = modifier ,
+            value = email,
+            onValueChange = { it -> email = it } ,
+            placeholder = { Text("Enter your EmailID")}
+        )
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    private fun Password( modifier : Modifier ){
+        var password by remember{ mutableStateOf("") }
+        TextField(
+            modifier = modifier,
+            value = password,
+            onValueChange = { it -> password = it}
+        )
+    }
+
 }
 
